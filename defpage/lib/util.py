67290ea769922dab7f0c12a7968b8201376f38c2
Ilshad Khabibullin <astoon@spacta.com>
2012-02-29 06:27:41 +0400

    epochz function

--- defpage/lib/util.py
+++ defpage/lib/util.py
@@ -1,6 +1,8 @@
 import json
 import random
 import string
+import time
+from datetime import datetime
 
 def random_string(length):
     chars = []
@@ -22,3 +24,13 @@ def serialized(k):
     def _set(inst, v):
         setattr(inst, k, json.dumps(v))
     return property(_get, _set)
+
+def epochz(dt):
+    """Converts given datetime into UTC with UNIX epoch timestump by integer.
+    epochz :: datetime.datetime() -> integer(unix(UTC))
+    Testing:
+    >>> int(datetime.utcnow().strftime("%s")) == epochz(datetime.now())
+    """
+    delta = int(time.mktime(time.gmtime()) - time.mktime(datetime.now().timetuple()))
+    epoch = int(time.mktime(dt.timetuple()))
+    return epoch + delta
